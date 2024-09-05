a2827e4411a8c4b075f5288a4ee37070fd3ec2f3
Remi Farine Rosendo <remi.farine@demprel.local>
2024-09-05 11:59:24 -0300

    Primeiro commit

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+			var consumoApi = new ConsumoApi();
+			var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=6585022c");
+			// System.out.println(json);
+			// json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+			System.out.println(json);
+	}
+
+}
