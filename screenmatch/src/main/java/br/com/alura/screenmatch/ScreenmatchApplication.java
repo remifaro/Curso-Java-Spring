800720bbea099eac9b540c63cd09182cd3ae5e62
remifaro <faroremi1@gmail.com>
2024-09-09 18:17:29 -0300

    wip: Alterando impressão de lista de episódios

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.principal.Principal;
+
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
@@ -15,12 +16,8 @@ public class ScreenmatchApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-			var consumoApi = new ConsumoApi();
-			var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=6585022c");
-			// System.out.println(json);
-			// json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-			System.out.println(json);
+			Principal principal = new Principal();
+			principal.exibeMenu();
 	}
 
 }
